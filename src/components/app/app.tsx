3675ee0400b6cbebcc9e60583f2326e1c882da92
rmaidveo <ilonazelenaya@gmail.com>
2022-03-17 19:11:05 +0300

    create app and basic setting

---
+++ src/components/app/app.tsx
@@ -0,0 +1,5 @@
+function App(): JSX.Element {
+  return <p> Начало тестового</p>
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.tsx',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+        open: true,
+        port: 1337,
+    },
+    module: {
+        rules: [
+        {
+            test: /\.(ts|tsx)$/,
+            exclude: /node_modules/,
+            use: {
+            loader: 'babel-loader',
+            },
+        }
+        ],
+    },
+   resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+},
+    devtool: 'source-map',
+};
